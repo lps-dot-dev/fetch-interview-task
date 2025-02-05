9c92240b4fb2876cba2c666392da41dea7db30d6
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-02-05 14:56:53 -0800

    Add some request JSON validation

--- receipts/types.go
+++ receipts/types.go
@@ -1,14 +1,14 @@
 package receipts
 
 type Receipt struct {
-	Retailer     string        `json:"retailer"`
-	PurchaseDate string        `json:"purchaseDate"`
-	PurchaseTime string        `json:"purchaseTime"`
-	Total        string        `json:"total"`
-	Items        []ReceiptItem `json:"items"`
+	Retailer     string        `json:"retailer" binding:"required"`
+	PurchaseDate string        `json:"purchaseDate" binding:"required"`
+	PurchaseTime string        `json:"purchaseTime" binding:"required"`
+	Total        string        `json:"total" binding:"numeric"`
+	Items        []ReceiptItem `json:"items" binding:"min=1"`
 }
 
 type ReceiptItem struct {
-	ShortDescription string `json:"shortDescription"`
-	Price            string `json:"price"`
+	ShortDescription string `json:"shortDescription" binding:"required"`
+	Price            string `json:"price" binding:"numeric"`
 }
