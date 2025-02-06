e1c869313f2ef0088254f1923c2441c7b5aecf05
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-02-05 16:55:05 -0800

    Fifth scoring rule
    If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.

--- receipts/process.go
+++ receipts/process.go
@@ -4,6 +4,7 @@ import (
 	"math"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9]+`)
@@ -31,6 +32,25 @@ func Process(receipt Receipt) (int, error) {
 
 	// 5 points for every two items on the receipt
 	score += 5 * int(len(receipt.Items)/2)
+
+	/**
+	 * If the trimmed length of the item description is a multiple of 3,
+	 * multiply the price by 0.2 and round up to the nearest integer.
+	 * The result is the number of points earned.
+	 */
+	for _, receiptItem := range receipt.Items {
+		trimmedDescription := strings.TrimSpace(receiptItem.ShortDescription)
+		if len(trimmedDescription)%3 != 0 {
+			continue
+		}
+
+		itemPrice, parsingError := strconv.ParseFloat(receiptItem.Price, 32)
+		if parsingError != nil {
+			return 0, parsingError
+		}
+
+		score += int(math.Ceil(itemPrice / 5))
+	}
 
 	return score, nil
 }
