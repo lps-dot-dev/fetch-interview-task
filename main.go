1082fd25c3f621cd59771a2d32b3079d6840939e
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-02-05 18:37:17 -0800

    Generate UUID based on receipt

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fetch/receipts"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -21,13 +24,26 @@ func processReceipt(c *gin.Context) {
 		return
 	}
 
-	receiptId, processingError := receipts.Process(receipt)
+	receiptScore, processingError := receipts.Process(receipt)
 	if processingError != nil {
 		c.AbortWithError(http.StatusBadRequest, processingError)
 		return
 	}
 
+	// We should account for duplicate receipts
+	// If the same receipt is given twice, we should return the same UUID
+	var receiptBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&receiptBuffer)
+	encodingError := encoder.Encode(receipt)
+	if encodingError != nil {
+		c.AbortWithError(http.StatusInternalServerError, encodingError)
+		return
+	}
+
+	uuid := uuid.NewSHA1(uuid.NameSpaceURL, receiptBuffer.Bytes())
+
 	c.JSON(http.StatusOK, gin.H{
-		"id": receiptId,
+		"id":    uuid.String(),
+		"score": receiptScore,
 	})
 }
